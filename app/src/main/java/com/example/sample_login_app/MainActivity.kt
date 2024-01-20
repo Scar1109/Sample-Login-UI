694135741edd1bf2d16941640f84dc43fab36006
Kaveen Dinethma <kaveen.dinethma@gmail.com>
2024-01-21 02:15:06 +0530

    Initial Commit

--- app/src/main/java/com/example/sample_login_app/MainActivity.kt
+++ app/src/main/java/com/example/sample_login_app/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.sample_login_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var usernameInput : EditText
+    private lateinit var passwordInput : EditText
+    private lateinit var loginBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+            Log.i("Test Credentials", "username : $username and Password : $password")
+        }
     }
 }
